06f23b7631326676994d1f4f39fc258727755f27
Julio <jafortti@gmail.com>
2014-07-05 22:21:30 -0300

    Se agregaron 3 clases, Ibeacon, ValoresIbeacon y BleWrapperUiCallbacks.java

---
+++ ProshopAPI/src/com/fortmin/proshopapi/ble/Ibeacon.java
@@ -0,0 +1,229 @@
+package com.fortmin.proshopapi.ble;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.util.Log;
+
+public class Ibeacon {
+	private Activity mParent = null;    
+	private boolean mConnected = false;
+	private BluetoothManager mBluetoothManager = null;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private BluetoothDevice  mBluetoothDevice = null;
+    private final Handler mTimerHandler = new Handler();
+    private boolean mTimerEnabled = false;
+    private ValoresIbeacon valores= new ValoresIbeacon("",0);
+	/* defines (in milliseconds) how often RSSI should be updated */
+    private static final int RSSI_UPDATE_TIME_INTERVAL = 15000; // 15 seconds
+
+    /* callback object through which we are returning results to the caller */
+    private BleWrapperUiCallbacks mUiCallback = null;
+    /* define NULL object for UI callbacks */
+    private static final BleWrapperUiCallbacks NULL_CALLBACK = new BleWrapperUiCallbacks.Null(); 
+    
+   
+    /* creates BleWrapper object, set its parent activity and callback object */
+    public Ibeacon(Activity parent, BleWrapperUiCallbacks callback) {
+    	this.mParent =parent;
+    	mUiCallback = callback;
+    	if(mUiCallback == null) {
+			mUiCallback = NULL_CALLBACK;
+		}
+    }
+    public Ibeacon(Activity parent){
+    	this.mParent =parent;
+    	//mUiCallback=new BleWrapperUiCallbacks.Null();
+    	
+    }
+    
+    public BluetoothManager           getManager() { return mBluetoothManager; }
+    public BluetoothAdapter           getAdapter() { return mBluetoothAdapter; }
+    public BluetoothDevice            getDevice()  { return mBluetoothDevice; }
+    public boolean                    isConnected() { return mConnected; }
+
+	/* run test and check if this device has BT and BLE hardware available */
+	public boolean checkBleHardwareAvailable() {
+		// First check general Bluetooth Hardware:
+		// get BluetoothManager...
+		final BluetoothManager manager = (BluetoothManager) mParent.getSystemService(Context.BLUETOOTH_SERVICE);
+		if(manager == null) {
+			return false;
+		}
+		// .. and then get adapter from manager
+		final BluetoothAdapter adapter = manager.getAdapter();
+		if(adapter == null) {
+			return false;
+		}
+		
+		// and then check if BT LE is also available
+		boolean hasBle = mParent.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
+		return hasBle;
+	}    
+
+	
+	/* before any action check if BT is turned ON and enabled for us 
+	 * call this in onResume to be always sure that BT is ON when Your
+	 * application is put into the foreground */
+	public boolean isBtEnabled() {
+		final BluetoothManager manager = (BluetoothManager) mParent.getSystemService(Context.BLUETOOTH_SERVICE);
+		if(manager == null) {
+			return false;
+		}
+		
+		final BluetoothAdapter adapter = manager.getAdapter();
+		if(adapter == null) {
+			return false;
+		}
+		
+		return adapter.isEnabled();
+	}
+	
+	/* start scanning for BT LE devices around */
+	public void startScanning() {
+        mBluetoothAdapter.startLeScan(mDeviceFoundCallback);
+	}
+	
+	
+	
+	
+	
+	/* stops current scanning */
+	public void stopScanning() {
+		mBluetoothAdapter.stopLeScan(mDeviceFoundCallback);	
+	}
+	
+    /* initialize BLE and get BT Manager & Adapter */
+    public boolean initialize() {
+    	
+        if (mBluetoothManager == null) {
+            mBluetoothManager = (BluetoothManager) mParent.getSystemService(Context.BLUETOOTH_SERVICE);
+            if (mBluetoothManager == null) {
+                return false;
+            }
+        }
+
+        if(mBluetoothAdapter == null) {
+			mBluetoothAdapter = mBluetoothManager.getAdapter();
+		}
+        if (mBluetoothAdapter == null) {
+            return false;
+        }
+        return true;    	
+    }
+
+  
+    
+   
+
+  
+
+    /* request new RSSi value for the connection*/
+    public void readPeriodicalyRssiValue(final boolean repeat) {
+    	mTimerEnabled = repeat;
+    	// check if we should stop checking RSSI value
+    	if(mConnected == false || mTimerEnabled == false) {
+    		mTimerEnabled = false;
+    		return;
+    	}
+    	
+    	mTimerHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				if( mBluetoothAdapter == null || mConnected == false)
+				{
+					mTimerEnabled = false;
+					return;
+				}
+												
+				// add call it once more in the future
+				readPeriodicalyRssiValue(mTimerEnabled);
+			}
+    	}, RSSI_UPDATE_TIME_INTERVAL);
+    }    
+    
+    /* starts monitoring RSSI value */
+    public void startMonitoringRssiValue() {
+    	readPeriodicalyRssiValue(true);
+    }
+    
+    /* stops monitoring of RSSI value */
+    public void stopMonitoringRssiValue() {
+    	readPeriodicalyRssiValue(false);
+    }
+    
+  
+    
+  
+    
+  
+
+  
+
+    
+    
+   
+
+   
+    
+   
+    
+    /* defines callback for scanning results */
+    /*
+    private final BluetoothAdapter.LeScanCallback mDeviceFoundCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+        	mUiCallback.uiDeviceFound(device, rssi, scanRecord);
+        }
+    };	   */
+    
+ 
+    private final BluetoothAdapter.LeScanCallback mDeviceFoundCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+        	
+        	valores.setValor_nombre(device.getName());
+        	valores.setValorRssi(rssi);
+        	valores.setScanRecord(scanRecord);
+        	
+    		
+        }
+    };
+    
+    public String darNombreBeacon(){
+    	return valores.getValor_nombre();
+    }
+    
+    public int darRssiBeacon(){
+    	return valores.darValorRssi();
+    }
+    
+    public int darMinor(){
+    	return valores.getMinor();
+    }
+    
+    public int darMajor(){
+    	return valores.getMajor();
+    }
+    
+    public String getProximityUuid(){
+    	return valores.getProximityUuid();
+    }
+    
+    public void calibrarBeacon(int rssi_ibeacon){
+    	valores.setCalibracion(rssi_ibeacon);
+    }
+    public float darDistancia(){
+    	return valores.getDistancia();
+    }
+    public double getTxPower(){
+    	return valores.getTxPower();
+    }
+    public boolean clienteCerca(){
+    	return valores.clienteCerca();
+    }
+}
---
+++ ProshopAPI/src/com/fortmin/proshopapi/ble/ValoresIbeacon.java
@@ -0,0 +1,164 @@
+package com.fortmin.proshopapi.ble;
+
+import java.util.UUID;
+
+
+//clase auxiliar creada por Fortti	
+
+public class ValoresIbeacon {
+  private static float A=100; // 1 metro expresado en centimetros para calculo de distancia	
+  private static float distancia_cercana=200; //definición de ambito de cobertura cercana son 200 cm
+  private String valor_nombre;
+  private String valor_uuid;
+  private int rssi,rssi_metro,rssi_ibeacon;
+  private byte[] scanRecord;
+  private int major, minor;
+  double txPower;
+  private  String proximityUuid;
+  private boolean calibrado;
+  private float ni,distancia;
+  private final int startByte=2;// para obtener los valores de major y minor
+  
+  public int getRssi() {
+	return rssi;
+}
+
+public void setRssi(int rssi) {
+	this.rssi = rssi;
+}
+
+public byte[] getScanRecord() {
+	return scanRecord;
+	
+}
+
+public void setScanRecord(byte[] scanRecord) {
+	this.scanRecord = scanRecord;
+	major = (scanRecord[startByte+23] & 0xff) * 0x100 + (scanRecord[startByte+24] & 0xff);
+	minor = (scanRecord[startByte+25] & 0xff) * 0x100 + (scanRecord[startByte+26] & 0xff);
+	txPower = (scanRecord[startByte+27] & 0xff) * 0x100 + (scanRecord[startByte+28] & 0xff);; // this one is signed
+	byte[] uuid_ent= new byte[16];
+	uuid_ent[0]=scanRecord[9];
+	uuid_ent[1]=scanRecord[10];
+	uuid_ent[2]=scanRecord[11];
+	uuid_ent[3]=scanRecord[12];
+	uuid_ent[4]=scanRecord[13];
+	uuid_ent[5]=scanRecord[14];
+	uuid_ent[6]=scanRecord[15];
+	uuid_ent[7]=scanRecord[16];
+	uuid_ent[8]=scanRecord[17];
+	uuid_ent[9]=scanRecord[18];
+	uuid_ent[10]=scanRecord[19];
+	uuid_ent[11]=scanRecord[20];
+	uuid_ent[12]=scanRecord[21];
+	uuid_ent[13]=scanRecord[22];
+	uuid_ent[14]=scanRecord[23];
+	uuid_ent[15]=scanRecord[24];
+	UUID uuid=UUID.nameUUIDFromBytes(uuid_ent);
+	this.proximityUuid=uuid.toString();
+}
+
+ValoresIbeacon(String nombre, int v_rssi){
+	  valor_nombre=nombre;
+	  this.rssi=v_rssi;
+	  this.calibrado=false;
+	  
+  }
+
+public String getValor_uuid() {
+	return valor_uuid;
+}
+
+public void setValor_uuid(String valor_uuid) {
+	this.valor_uuid = valor_uuid;
+}
+
+public String getValor_rssi() {
+	return String.valueOf(rssi);
+}
+
+
+public String getValor_nombre() {
+	return valor_nombre;
+}
+
+public void setValor_nombre(String valor_nombre) {
+	this.valor_nombre = valor_nombre;
+}
+
+public int darValorRssi(){
+	return rssi;
+}
+public void setValorRssi(int valor){
+	if (rssi==0)
+	 rssi=valor;
+	else {
+		int promedio=(valor+rssi)/2;
+		rssi=promedio;
+	}
+}
+
+public int getMajor() {
+	return major;
+}
+
+public void setMajor(int major) {
+	this.major = major;
+}
+
+public int getMinor() {
+	return minor;
+}
+
+public void setMinor(int minor) {
+	this.minor = minor;
+}
+
+public double getTxPower() {
+	return txPower;
+}
+
+public void setTxPower(int txPower) {
+	this.txPower = txPower;
+}
+
+public String getProximityUuid() {
+	return proximityUuid;
+}
+
+public void setProximityUuid(String proximityUuid) {
+	this.proximityUuid = proximityUuid;
+}
+public void setCalibracion(int rssi_ibeacon){
+	// potencia es el valor de rssi captado por el movil  a la distancia de 1 metro
+	// rssi_ibeacon es el rssi que tiene el dispositivo que transmite
+	
+	this.rssi_ibeacon=rssi_ibeacon;
+	this.distancia=Math.abs(rssi_ibeacon);// tomo como referencia el rssi a la distancia requerida
+	this.calibrado=true;
+	//this.ni=(float) ((-rssi_ibeacon-rssi_metro)/(10*Math.log10(A)));
+	
+}
+public float getDistancia(){
+	
+	return distancia;
+}
+public boolean estaCalibrado(){
+	return this.calibrado;
+}
+public boolean clienteCerca(){
+	return (Math.abs(rssi) <=distancia);
+}
+public float calculateAccuracy() {
+	 
+
+	  double ratio = rssi*1.0/this.rssi_metro;
+	/*  if (ratio < 1.0) {
+	    return (float) Math.pow(ratio,10);
+	  }
+	  else {*/
+	    double accuracy =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
+	    return (float) accuracy;
+	//  }
+	}   
+}
